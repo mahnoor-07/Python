445097a54a0a19f3f975dcc9950e0cdec0d2593f
Mahnoor Fatima <n4910813@gmail.com>
2025-11-29 19:43:30 -0800

    Bisection Search Algorithm

--- Fast_square_root.py
+++ Fast_square_root.py
@@ -1,41 +1,41 @@
 # # Lecture 6 - Bisection Search
 
-# # Fast Square Root
-# x = 36
-# epsilon = 0.01
-# num_guess = 0
+# Fast Square Root
+x = 36
+epsilon = 0.01
+num_guess = 0
 
-# # initialize endpoints and guess
-# low = 0
-# high = x
-# guess = (high + low)/2.0
+# initialize endpoints and guess
+low = 0
+high = x
+guess = (high + low)/2.0
 
-# while abs(guess**2 - x) >= epsilon:
-#     # what is repeated each time?
-#     if guess**2 < x:
-#         low = guess 
-#     else:
-#         high = guess
-#     guess = (high + low)/2.0
-#     # Make a new guess using the new endpoints
-#     num_guess += 1
-# print("num_guesses = ", num_guess)
-# print(guess, "is close to square root of", x)
+while abs(guess**2 - x) >= epsilon:
+    # what is repeated each time?
+    if guess**2 < x:
+        low = guess 
+    else:
+        high = guess
+    guess = (high + low)/2.0
+    # Make a new guess using the new endpoints
+    num_guess += 1
+print("num_guesses = ", num_guess)
+print(guess, "is close to square root of", x)
 
-# # Find the cube root of positive cubes with in epsilon
-# cube = 27 
-# epsilon = 0.01
-# low = 0
-# high = cube
-# guess = (high+low)/2
-# while abs(guess**3-cube) > epsilon:
-#     if guess**3 > cube:
-#         high = guess 
-#     else:
-#         low = guess
-#     guess = (high + low)/2
+# Find the cube root of positive cubes with in epsilon
+cube = 27 
+epsilon = 0.01
+low = 0
+high = cube
+guess = (high+low)/2
+while abs(guess**3-cube) > epsilon:
+    if guess**3 > cube:
+        high = guess 
+    else:
+        low = guess
+    guess = (high + low)/2
 
-# print(guess)
+print(guess)
 
 # Newton-Raphson Root Finder
 epsilon = 0.01
